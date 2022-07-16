cfd9099330ed9657d3a24182db37e1d4f0fcb9ad
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-07-16 17:24:01 +0200

    added database schema

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,3 +1,13 @@
-CREATE TABLE IF NOT EXISTS messages (
+CREATE TABLE IF NOT EXISTS "message" (
+    "message_id"   VARCHAR(60)  DEFAULT RANDOM_UUID() PRIMARY KEY,
+    "user_id"      VARCHAR(60),
+    "content"      VARCHAR      NOT NULL,
+    "content_type" VARCHAR(128) NOT NULL,
+    "sent"         TIMESTAMP    NOT NULL
+);
 
+CREATE TABLE IF NOT EXISTS "user" (
+    "user_id"      VARCHAR(60)  DEFAULT RANDOM_UUID() PRIMARY KEY,
+    "last_seen_id" VARCHAR(60),
+    "username"     VARCHAR(60)  NOT NULL
 );
